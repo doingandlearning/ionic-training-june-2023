befca18b68b86342bdf97e504cef7b9ab9966426
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-06-05 14:28:44 +0100

    Start of the store.

---
+++ 1-myApp/src/views/BirdDetailPage.vue
@@ -0,0 +1,19 @@
+<template>
+	<layout pageTitle="Bird" pageBackLink="/birds">
+		Info about a bird!
+	</layout>
+</template>
+
+<script setup>
+import Layout from "../components/Layout.vue"
+
+// hooks -> composables
+import {useRoute} from "vue-router"
+
+const route = useRoute();
+
+console.log(route.params.id)
+
+birds.find(bird => bird.id === route.params.id)
+
+</script>
