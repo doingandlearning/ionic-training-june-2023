357fa121e61089707f2bc7623226645fb7f575c0
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-06-06 12:44:20 +0100

    Does something secret.

--- 2-firebase-todo/src/firebase/index.ts
+++ 2-firebase-todo/src/firebase/index.ts
@@ -3,14 +3,7 @@ import { getFirestore, collection } from "firebase/firestore";
 import { createApp } from "vue";
 import { useCollection } from "vuefire";
 
-export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDokmawDDLm3OITrekZh0YdG8qGrYWLfx8",
-  authDomain: "ionic-firebase-chat-59ba3.firebaseapp.com",
-  projectId: "ionic-firebase-chat-59ba3",
-  storageBucket: "ionic-firebase-chat-59ba3.appspot.com",
-  messagingSenderId: "806201131469",
-  appId: "1:806201131469:web:ed03fd7e94d485ee734484",
-});
+export const firebaseApp = initializeApp();
 
 export const firestore = getFirestore(firebaseApp);
 
